d347939f4777971130f831b5c30796cf4367b0da
ksauer <ksauer@w8722fd0x91.(none)>
2010-03-08 18:53:57 -0800

    updated path

--- experiments/client/performance/logparsing/src/main/scripts/training-and-validation-preprocessing.sh
+++ experiments/client/performance/logparsing/src/main/scripts/training-and-validation-preprocessing.sh
@@ -7,6 +7,7 @@
 # $2:  # validation runs
 # $3:  starting thread of interest
 # $4:  ending thread of interest
+# $5:  parser's directory
 
 # DEINTERLEAVING => One file/run -> one file/thread
 mkdir -p $1/training-logs
@@ -25,11 +26,11 @@ done
 
 # PARSING => CSV per thread
 echo Parsing training per-thread logs...
-java -DlogDir=$1/training-logs -cp $MY_SCADS/logparsing/target/logparsing-1.0-SNAPSHOT-jar-with-dependencies.jar parser.ParsePerThreadLogs
+java -DlogDir=$1/training-logs -cp $5/logparsing-1.0-SNAPSHOT-jar-with-dependencies.jar parser.ParsePerThreadLogs
 
 #for i in `jot $2 1 $2`;
 for i in `seq 1 $2`
 do echo Parsing validation $i per-thread logs...;
-java -DlogDir=$1/validation-logs/validation$i-logs -cp $MY_SCADS/logparsing/target/logparsing-1.0-SNAPSHOT-jar-with-dependencies.jar parser.ParsePerThreadLogs;
+java -DlogDir=$1/validation-logs/validation$i-logs -cp $5/logparsing-1.0-SNAPSHOT-jar-with-dependencies.jar parser.ParsePerThreadLogs;
 done
 
