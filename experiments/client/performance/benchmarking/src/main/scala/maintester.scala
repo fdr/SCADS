d5905d1472978b169b4a6c06dd08f507443572bf
Kristal Curtis <ksauer@berkeley.edu>
2010-05-28 13:33:37 -0700

    added pkg

--- experiments/client/performance/benchmarking/src/main/scala/maintester.scala
+++ experiments/client/performance/benchmarking/src/main/scala/maintester.scala
@@ -1,3 +1,5 @@
+package edu.berkeley.cs.scads.benchmarking
+
 object MainTester extends Application {
 	val whichOp = System.getProperty("whichOp").toInt			// only one at a time => take advantage of ||ism using EC2
 	println("whichOp=>" + whichOp + "<")
