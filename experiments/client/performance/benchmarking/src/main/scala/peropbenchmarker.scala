3b94775709845f8c9f8457d48ec1e2ce15f31686
Kristal Curtis <ksauer@berkeley.edu>
2010-05-28 13:35:03 -0700

    updated peropbenchmarker to use new version of piql compiler

--- experiments/client/performance/benchmarking/src/main/scala/peropbenchmarker.scala
+++ experiments/client/performance/benchmarking/src/main/scala/peropbenchmarker.scala
@@ -1,15 +1,21 @@
+package edu.berkeley.cs.scads.benchmarking
+
 //import edu.berkeley.cs.scads.model._
 import edu.berkeley.cs.scads.piql._
 import edu.berkeley.cs.scads.piql.parser._
 
+import piql._
+
 import org.apache.log4j._
 import org.apache.log4j.Level._
-import edu.berkeley.cs.scads.thrift._
+//import edu.berkeley.cs.scads.thrift._
 import java.lang.Math
 import scala.collection.mutable._
 
+import org.apache.avro.specific.SpecificRecordBase
 
-object PerOpBenchmarker {
+
+object PerOpBenchmarker extends QueryExecutor {
 	def benchmarkOp(opType:Int, aSize:Int, bSize:Int, numA:Int, numB:Int, maxItemsA:Int, maxItemsB:Int)(implicit env: Environment) = {
 		opType match {
 			case 1 => benchmarkOp1(maxItemsA)
@@ -29,7 +35,7 @@ object PerOpBenchmarker {
 	// Util functions
 	protected def getRandomPKForA(maxItems:Int):String = {
 		val rand = new java.util.Random
-		"key-" + (rand.nextInt(maxItems) + 1)
+		"key1-" + (rand.nextInt(maxItems) + 1)
 	}
 	
 	// secondary key
@@ -41,39 +47,53 @@ object PerOpBenchmarker {
 	
 	// Benchmarking functions
 	protected def benchmarkOp1(maxItems:Int)(implicit env: Environment) = {
-		val rand = new java.util.Random
-		(new A).singleGet("ent_A", StringField.apply(getRandomPKForA(maxItems)), ReadRandomPolicy)
-		// add 1 b/c nextInt => [0,maxItems), but I want [1,maxItems]
+		//singleGet("ent_A", StringField.apply(getRandomPKForA(maxItems)), ReadRandomPolicy)
+		singleGet("ent_A", List(BoundStringValue( getRandomPKForA(maxItems) )))
 	}
 
 	// maxItems => which A should be the owner of all the B's; 
 	// numB => how many B's should I return
 	protected def benchmarkOp2(numB:Int, maxItems:Int)(implicit env: Environment) = {
-		(new A).prefixGet("ent_B", StringField.apply(getRandomPKForA(maxItems)), IntegerField.apply(numB), true, ReadRandomPolicy)
+		//prefixGet("ent_B", StringField.apply(getRandomPKForA(maxItems)), IntegerField.apply(numB), true, ReadRandomPolicy)
+		val key = new A.KeyType
+		key.key1 = getRandomPKForA(maxItems)
+		prefixGet("ent_B", List(BoundAvroRecordValue( key )), BoundIntegerValue(numB), true)
+		
 	}
 	
+	
+	// CAN'T be used till indexes are created
+	// When it works, figure out how to creat the tupleStream 
+	// How? call userByEmail and see what prefixGet returns 
+	//
 	// maxItems => what to use for the secondary key;
 	// numA => what to use for the limit (= how many of the entries sharing this SK should I return)
 	protected def benchmarkOp3(numA:Int, maxItems:Int)(implicit env: Environment) = {
+		/*
 		// Generate TupleStream
 		val secondaryKey = getRandomSKForA(maxItems)
-		var tupleStream:List[Tuple] = Nil
+		//var tupleStream:List[Tuple] = Nil
+		var tupleStream:List[(SpecificRecordBase, SpecificRecordBase)] = Nil
+		
 		(1 to numA).foreach((i) => {
 			val primaryKey = "key-" + i
 			val field = new CompositeField(StringField.apply(secondaryKey) :: StringField.apply(primaryKey) :: Nil, 
 				classOf[StringField].asInstanceOf[Class[Field]] :: classOf[StringField].asInstanceOf[Class[Field]] :: Nil)
-			val newTuple = (new Tuple(field, new IntegerVersion, StringField.apply(primaryKey).serializeKey))
+			//val newTuple = (new Tuple(field, new IntegerVersion, StringField.apply(primaryKey).serializeKey))
 
-			tupleStream = newTuple :: tupleStream
+			//tupleStream = newTuple :: tupleStream
+			tupleStream = ()
 		})
 		tupleStream = tupleStream.reverse
 		
 		// Call op
 		try {
-			(new A).sequentialDereferenceIndex("ent_A", ReadRandomPolicy, tupleStream)
+			//sequentialDereferenceIndex("ent_A", ReadRandomPolicy, tupleStream)
+			sequentialDereferenceIndex("ent_A", tupleStream)
 		} catch {
 			case e:AssertionError => e.printStackTrace()
 		}
+		*/
 	}
 	
 	// doesn't need maxItems b/c assumes that numA & numB are less than maxItems;
@@ -86,43 +106,74 @@ object PerOpBenchmarker {
 
 		(1 to numA).foreach((i) => {
 			val a = new A
-			a.key("key-" + i)
-			a.key2("key2-" + i)
-			a.dataA(PerOpDataGen.generateDataField(aSize))
+			a.key1 = ("key1-" + i)
+			a.key2 = ("key2-" + i)
+			a.dataA = PerOpDataGen.generateDataField(aSize)
 
 			entityStream = a :: entityStream
 		})
 		entityStream = entityStream.reverse
 		
 		// Call op
-		(new A).prefixJoin("ent_B", List(AttributeCondition("key")), IntegerField.apply(numB), true, ReadRandomPolicy, entityStream)
+		//prefixJoin("ent_B", List(AttributeCondition("key")), IntegerField.apply(numB), true, ReadRandomPolicy, entityStream)
+		prefixJoin("ent_B", List(AttributeCondition("key1")), BoundIntegerValue(numB), true, entityStream)
+		
 	}
 
+	/*
 	// aSize, bSize => length of dataA & dataB fields (respectively);
 	// numB => length of entityStream;
 	// maxItemsA&B => how to set key, key2
+	(1 to maxItemsA).foreach((i) => {
+		val a = new A
+		a.key1 = ("key1-" + i)
+		a.key2 = ("key2-" + i)
+		a.dataA = generateDataField(aSize)
+		a.save
+		
+		(1 to maxItemsB).foreach((j) => {
+			val b = new B
+			b.owner = a
+			b.key2 = ("key2-" + j)
+			b.dataB = generateDataField(bSize)
+			b.save
+
+			//if ((j % 100) == 0)
+				println("Added " + j + " items of type B, owned by A:key" + i + "...")
+		})
+		
+		//if ((i % 100) == 0)
+			println("Added " + i + " items of type A...")
+	})
+	*/
+	
 	protected def benchmarkOp5(aSize:Int, bSize:Int, numB:Int, maxItemsA:Int, maxItemsB:Int)(implicit env: Environment) = {
 		var entityStream:List[B] = Nil
+
+		println("Namespace has data? " + env.namespaces("ent_A").getRange(null, null))
 
 		// Generate EntityStream
 		(1 to numB).foreach((i) => {
 			val a = new A
-			val key = getRandomPKForA(maxItemsA)
-			a.key(key)
-			a.key2(key)		// dummy value
-			a.dataA(PerOpDataGen.generateDataField(aSize))
+			//val key = getRandomPKForA(maxItemsA)
+			val keyNum = ((new java.util.Random).nextInt(maxItemsA) + 1)
+			a.key1 = ("key1-" + keyNum)
+			a.key2 = ("key2-" + keyNum)
+			a.dataA = PerOpDataGen.generateDataField(aSize)
 
 			val b = new B
-			b.key2(getRandomSKForA(maxItemsB))	// same format as for A's SK
-			b.dataB(PerOpDataGen.generateDataField(bSize))
-			b.owner(a)
+			b.owner = a
+			b.key2 = getRandomSKForA(maxItemsB)	// same format as for A's SK
+			b.dataB = PerOpDataGen.generateDataField(bSize)
 
 			entityStream = b :: entityStream
 		})
 		entityStream = entityStream.reverse	
 
 		// Call Op
-		(new A).pointerJoin("ent_A", List(AttributeCondition("owner")), ReadRandomPolicy, entityStream)
+		//pointerJoin("ent_A", List(AttributeCondition("owner")), ReadRandomPolicy, entityStream)
+		pointerJoin("ent_A", List(AttributeCondition("owner")), entityStream)
+		
 	}
 	
 	
@@ -130,21 +181,24 @@ object PerOpBenchmarker {
 	// numA => length of TupleStream;
 	// maxItems => what to use for A's key, key2
 	protected def benchmarkOp6(aSize:Int, numA:Int, maxItems:Int)(implicit env: Environment) = {
-		var tupleStream:List[Tuple] = Nil
+		//var tupleStream:List[Tuple] = Nil
+		//var tupleStream:TupleStream = Nil
+		var tupleStream:List[(SpecificRecordBase, SpecificRecordBase)] = Nil
 		
 		// Generate TupleStream
 		(1 to numA).foreach((i) => {
 			val a = new A
-			a.key(getRandomPKForA(maxItems))
-			a.key2(getRandomSKForA(maxItems))
-			a.dataA(PerOpDataGen.generateDataField(aSize))
+			a.key1 = getRandomPKForA(maxItems)
+			a.key2 = getRandomSKForA(maxItems)
+			a.dataA = PerOpDataGen.generateDataField(aSize)
 			
-			tupleStream = (new Tuple(a.key, new IntegerVersion, a.serializeAttributes)) :: tupleStream 
+			//tupleStream = (new Tuple(a.key1, new IntegerVersion, a.serializeAttributes)) :: tupleStream 
+			tupleStream = (a.key, a.value) :: tupleStream
 		})
 		tupleStream = tupleStream.reverse
 		
 		// Call Op
-		(new A).materialize(classOf[A], tupleStream)
+		materialize(classOf[A].asInstanceOf[Class[edu.berkeley.cs.scads.piql.Entity[_,_]]], tupleStream)
 	}
 	
 	
@@ -174,19 +228,20 @@ object PerOpBenchmarker {
 
 		(1 to numA).foreach((i) => {
 			val a = new A
-			a.key("key-" + i)
+			a.key1 = ("key-" + i)
 			if (whichEntitiesShareSK.exists(s => s == i))
-				a.key2(sharedSK)
+				a.key2 = sharedSK
 			else
-				a.key2("key2-other")
-			a.dataA(PerOpDataGen.generateDataField(aSize))
+				a.key2 = "key2-other"
+			a.dataA = PerOpDataGen.generateDataField(aSize)
 
 			aEntitySeq = a :: aEntitySeq
 		})
 		aEntitySeq = aEntitySeq.reverse
 		
 		// Call op
-		(new A).selection(HashMap(("key2", StringField.apply(sharedSK))).asInstanceOf[HashMap[String,Field]], aEntitySeq)
+		selection(HashMap(("key2", BoundStringValue(sharedSK))).asInstanceOf[HashMap[String,BoundValue]], aEntitySeq)
+		
 	}
 	
 	
@@ -206,16 +261,16 @@ object PerOpBenchmarker {
 				pkList = pk :: pkList
 				
 				val a = new A
-				a.key(pk)
-				a.key2(getRandomSKForA(maxItems))
-				a.dataA(PerOpDataGen.generateDataField(aSize))
+				a.key1 = pk
+				a.key2 = getRandomSKForA(maxItems)
+				a.dataA = PerOpDataGen.generateDataField(aSize)
 				
 				aEntitySeq = a :: aEntitySeq
 			}
 		}
 		
 		// Call op
-		(new A).sort(List("key"), true, aEntitySeq)
+		sort(List("key1"), true, aEntitySeq)
 	}
 
 
@@ -228,9 +283,9 @@ object PerOpBenchmarker {
 		
 		(1 to numA).foreach((i) => {
 			val a = new A
-			a.key(getRandomPKForA(maxItems))
-			a.key2(getRandomSKForA(maxItems))
-			a.dataA(PerOpDataGen.generateDataField(aSize))
+			a.key1 = getRandomPKForA(maxItems)
+			a.key2 = getRandomSKForA(maxItems)
+			a.dataA = PerOpDataGen.generateDataField(aSize)
 			
 			aEntitySeq = a :: aEntitySeq
 		})
@@ -240,7 +295,8 @@ object PerOpBenchmarker {
 		val K = (new java.util.Random).nextInt(numA) + 1 	// add 1 so range is [1,numA]; otherwise, it's [0,numA)
 		
 		// Call op
-		(new A).topK(IntegerField(K), aEntitySeq)
+		//topK(IntegerField(K), aEntitySeq)
+		topK(BoundIntegerValue(K), aEntitySeq)
 	}
 	
 }
